e5297394c8078298d7eaf64f72d07f4c30d450ab
Lwando92 <lwando@kineticskunk.com>
2019-01-25 15:58:34 +0200

    styles

--- db.py
+++ db.py
@@ -5,16 +5,16 @@ import os
 
 db.create_all()
 
-# Data to initialize database with
-Data = [
-	{'name'		: 'Darren'	,'physics': 1, 'maths': 1, 'chemistry':1},
-	{'name'		: 'Kent'	,'physics': 1, 'maths': 1, 'chemistry':1},
-	{'name'		: 'Bunny'	,'physics': 1, 'maths': 1, 'chemistry':1}
-]
+# # Data to initialize database with
+# Data = [
+# 	{'name'		: 'Darren'	,'physics': 1, 'maths': 1, 'chemistry':1},
+# 	{'name'		: 'Kent'	,'physics': 1, 'maths': 1, 'chemistry':1},
+# 	{'name'		: 'Bunny'	,'physics': 1, 'maths': 1, 'chemistry':1}
+# ]
 
 # Iterate over the PEOPLE structure and populate the database
-for xData in Data:
-	d = Student(name=xData['name'], physics=xData['physics'],maths=xData['maths'], chemistry=xData['chemistry'])
-	db.session.add(d)
+# for xData in Data:
+# 	d = Student(name=xData['name'], physics=xData['physics'],maths=xData['maths'], chemistry=xData['chemistry'])
+# 	db.session.add(d)
 
 db.session.commit()
